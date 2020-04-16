25316290b590f6dba0cc1edfacd43d94662feec2
gauravm-bit <gauravwrk01@gmail.com>
2020-04-16 10:51:58 +0530

    ADDED:test case to check fare and method from it.

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,5 +1,12 @@
 public class InvoiceGenerator {
+    private static final double COST_PER_KILOMETER = 10;
+    private static final int COST_PER_MINUTE = 1;
+
     public static void main(String[] args) {
         System.out.println("welcome");
+    }
+
+    public double calculateFare(double distance, int time) {
+        return distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
     }
 }
