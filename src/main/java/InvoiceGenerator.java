b560f634c65d6a0ffffc6cf78bec68d44d2fbc3d
gauravm-bit <gauravwrk01@gmail.com>
2020-04-16 11:30:02 +0530

    REFACTOR:test case for minimum fare and test-case to check it.

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,12 +1,16 @@
 public class InvoiceGenerator {
     private static final double COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final double MIN_FARE = 5;
 
     public static void main(String[] args) {
         System.out.println("welcome");
     }
 
     public double calculateFare(double distance, int time) {
-        return distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        double fare =  distance* COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        if(fare < MIN_FARE)
+            return MIN_FARE;
+        return fare;
     }
 }
